fb78372c455a4a4bac0ce23d68ca89c8deddba2b
Vladimir <valenkin@yandex.ru>
2023-01-26 01:16:16 +0500

    task2

---
+++ task2/task2.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Задача 2: определить, присутствует ли в заданном массиве, некоторое число");
+Console.WriteLine("Введите количество элементов массива");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = new int[size];
+for (int index = 0; index < size; index++)
+{
+    array[index] = new Random().Next(-100, 100);
+    Console.Write(array[index] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Введите искомое число");
+int number = int.Parse(Console.ReadLine()!);
+int count = 0;
+for (int index = 0; index < size; index++)
+{
+    if (number == array[index])
+    {
+        count = count + 1;
+    }
+
+}
+if (count > 0)
+{
+    Console.Write("Искомое число присутствует в массиве");
+}
+ else 
+ {
+    Console.Write("Искомое число не присутствует в массиве");
+ }
