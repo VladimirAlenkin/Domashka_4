de4202cab87d594dcffdf00721424a6ad6ab7548
Vladimir <valenkin@yandex.ru>
2023-01-26 00:39:46 +0500

    task1

---
+++ task1/task1.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача 1: задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран");
+int[] array = new int[8];
+for (int index = 0; index < 8; index++)
+{
+    array[index] = new Random().Next(1, 100);
+    if (array[index]%2 > 0)
+    {
+        array[index] = 1;
+    }
+    else
+    {
+        array[index] = 0;
+    }
+
+    Console.Write(array[index] + " ");
+}
